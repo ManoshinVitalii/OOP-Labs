0324432c4cb05730f9d6a4cd281338ebdb6ed844
ManoshinVitalii <146201623+ManoshinVitalii@users.noreply.github.com>
2023-11-18 18:37:23 +0300

    Create point.h

---
+++ Lab_3/point.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+const double EPS = 0.01;
+const double PI = acos(-1);
+
+class Point final {
+    private:
+        double x;
+        double y;
+    public:
+        Point();
+        Point(double _x, double _y);
+        Point(const Point &other);
+        Point &operator = (const Point &other);
+        virtual ~Point();
+        double getX() const;
+        double getY() const;
+        double distance(const Point &other) const;
+        bool operator == (const Point &other) const;
+        bool operator != (const Point &other) const;    
+        friend ostream& operator << (ostream &os, const Point& point);
+        friend istream& operator >> (istream& is, Point& point);
+        friend Point getAv(const vector<Point> &p);
+        friend bool operator < (const Point &point1, const Point &point2);
+        friend Point operator + (const Point &point1, const Point &point2);
+        friend Point operator - (const Point &point1, const Point &point2);
+        bool iszero();
+};
