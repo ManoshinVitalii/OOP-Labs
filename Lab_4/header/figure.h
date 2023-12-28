96e96347bea8defb6ab8f5cf6fd6a948842f6d45
ManoshinVitalii <146201623+ManoshinVitalii@users.noreply.github.com>
2023-12-28 15:59:39 +0300

    Create figure.h

---
+++ Lab_4/header/figure.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "point.h"
+
+template <typename T>
+class Figure {
+    protected:
+        virtual bool isEqual(const Figure &other) const = 0;
+        virtual bool valid(const vector<Point<T>> &p) const = 0;
+        vector<Point<T>> points;
+    public :
+        Figure();
+        Figure(const Figure &other);
+        Figure(const Figure &&other) noexcept;
+        ~Figure() = default;
+        vector<Point<T>> getPoints() const;
+        virtual operator double () const = 0;
+        friend bool operator==(const Figure &lhs, const Figure &rhs) {
+            if (typeid(lhs) != typeid(rhs)) {
+                return 0;
+            }
+            return lhs.isEqual(rhs);
+        }
+        virtual Point<T> getCenter() const = 0;
+        friend ostream &operator << (ostream& os, const Figure<T>& f) {
+            f.print(os);
+            return os;
+        }
+        friend istream &operator >> (istream& is, Figure<T>& f) {
+            f.input(is);
+            return is;
+        }
+};
+
+template <typename T>
+Figure<T>::Figure() {
+    vector<Point<T>> vpoints;
+    points = vpoints;
+}
+
+template <typename T>
+Figure<T>::Figure(const Figure<T> &other) : points(other.points) {}
+
+template <typename T>
+Figure<T>::Figure(const Figure<T> &&other) noexcept{
+    points = move(other.points);
+}
+
+template <typename T>
+vector<Point<T>> Figure<T>::getPoints() const{
+    return points;
+}
